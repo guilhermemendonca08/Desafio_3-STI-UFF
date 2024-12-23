f9e9134c212d0c4c40a0ca067157102660151e72
guilhermemendonca08 <guilhermepm@id.uff.br>
2024-12-23 12:58:16 -0300

    Espaçamento

--- aluno.rb
+++ aluno.rb
@@ -1,15 +1,15 @@
 class Aluno
-	attr_accessor :matricula, :cr, :notas, :disciplinas_cursadas
+  attr_accessor :matricula, :cr, :notas, :disciplinas_cursadas
     
-	def initialize(matricula)
-  	@matricula = matricula
-  	@cr = 0.0
-  	@disciplinas_cursadas = []
+  def initialize(matricula)
+    @matricula = matricula
+    @cr = 0.0
+    @disciplinas_cursadas = []
   end
 
   def calcular_cr
     total_carga_horaria = 0
-      @disciplinas_cursadas.each do |disciplina|
+    @disciplinas_cursadas.each do |disciplina|
       @cr += (disciplina.nota * disciplina.carga_horaria)
       total_carga_horaria += disciplina.carga_horaria
     end
